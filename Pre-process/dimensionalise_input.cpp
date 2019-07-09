4152215d2a70a127b77e84bf3d65790c4cbc9bcf
aditya12398 <aditya12398@gmail.com>
2019-07-10 00:55:45 +0530

    Modified the code for flatplate

--- Pre-process/dimensionalise_input.cpp
+++ Pre-process/dimensionalise_input.cpp
@@ -26,7 +26,10 @@ struct Cell *cell;
 struct Edge * edge;
 int inputdata(string fname)
 {
+    int temp;
+    double rho, u1, u2, pr;
     ifstream infile(fname);
+    //ifstream infile2("primitive-vector_implicit.dat");
     //Input Cell data
     if (!infile)
         return 1;
@@ -43,6 +46,7 @@ int inputdata(string fname)
     for (int k = 1; k <= max_cells; k++)
     {
         infile >> cell[k].cx >> cell[k].cy >> cell[k].rho >> cell[k].u1 >> cell[k].u2 >> cell[k].pr >> cell[k].area >> cell[k].noe;
+        //infile2 >> temp >> cell[k].rho >> cell[k].u1 >> cell[k].u2 >> cell[k].pr;
         //Set enclosing edges
         cell[k].edge = new int[cell[k].noe + 1];
         for (int r = 1; r <= cell[k].noe; r++)
@@ -93,17 +97,17 @@ int fixdata(string fname)
 int main()
 {
     int error = 0;
-    error = inputdata("ogrid_viscous");
+    error = inputdata("./Flatplate/flatplate_viscous_rectangular");
     if (error)
     {
-        cout << "Error in reading file, please check the file or file title\nExiting...";
+        cout << "Error in reading file, please check the file or file title\nExiting...\n";
         exit(1);
     }
     error = 0;
-    error = fixdata("ogrid_viscous_dim");
+    error = fixdata("./Flatplate/flatplate_viscous_rectangular_dim");
     if (error)
     {
-        cout << "Error in writing file, please check the file or file title\nExiting...";
+        cout << "Error in writing file, please check the file or file title\nExiting...\n";
         exit(1);
     }
     return 0;
